37cdeda97a7f3e15a9e811986f39f912bae704f4
Nikola Jaksic <nnjaksic@mines.edu>
2022-12-08 18:08:29 -0700

    added interesting (random noise) effect to ground shader

--- shaders/lab06.f.glsl
+++ shaders/lab06.f.glsl
@@ -10,8 +10,17 @@ in vec2 fragTextureCord;
 // fragment outputs
 out vec4 fragColorOut;
 
+
+float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
 void main() {
     vec4 fragColor = texture(textureMap, fragTextureCord);
+
+    //fun experimentations:
+    fragColor = fragColor * rand(fragTextureCord);
+    fragColor.w = 1.0f;
 
     fragColorOut = fragColor;
 }
--- shaders/lab06.v.glsl
+++ shaders/lab06.v.glsl
@@ -12,10 +12,15 @@ in vec3 vNormal;
 // varying outputs
 out vec2 fragTextureCord;
 
-
+float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
 
 void main() {
     gl_Position = mvpMatrix * vec4(vPos, 1.0);
+
+    //fun experimentations:
+    //gl_Position.y = gl_Position.y * rand(vec2(vPos.x,vPos.z));
 
 
     vec3 normalVector = vNormal;
